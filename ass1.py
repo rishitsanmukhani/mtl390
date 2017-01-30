4fbcfc993d30e8d819cfb39fabf1ac4118f0ac71
Rishit_India <rishitsanmukhani@gmail.com>
2017-01-30 15:39:22 +0530

    comments

--- ass1.py
+++ ass1.py
@@ -2,8 +2,10 @@ import numpy as np
 import scipy.stats as st
 import matplotlib.pyplot as plt
 
+# Reading data
 data = np.genfromtxt('2013CS10255_SANMUKHANI RISHIT GOPAL.csv')
 
+# Generating frequency table
 def frequency_table(x, nbins):
   print("Frequency Table:")
   hist, bins = np.histogram(x, nbins)
@@ -11,6 +13,7 @@ def frequency_table(x, nbins):
     print("{} - {} = {}".format(bins[idx], bins[idx+1], hist[idx]))
   print("\n")
 
+# Generating histogram plot
 def histogram_plot(x, nbins):
   print("Generating histogram plot...")
   plt.hist(x, nbins)
@@ -22,12 +25,15 @@ def histogram_plot(x, nbins):
   plt.close()  
   print("Done\n")
 
+# Generating bar plot
 def bar_plot(x,nbins):
   print("Generating bar plot...")
   plt.hist(x, nbins, histtype='bar', rwidth=0.4)
   plt.title("Bar Plot")
   plt.ylabel("Frequency")
 
+  # Adding the labels to each bar
+  # The labes represent the interval
   hist, bins = np.histogram(x, nbins)
   bins = bins + ((bins[1]-bins[0])/2)
   labels = ['']*nbins
@@ -40,6 +46,7 @@ def bar_plot(x,nbins):
   plt.close()
   print("Done\n")
 
+# Generating box plot
 def box_plot(x):
   print("Generating box plot...")
   plt.boxplot(x)
@@ -49,6 +56,7 @@ def box_plot(x):
   plt.close()
   print("Done\n")
 
+# Calculating the measures using scipy.stats
 def measures(x):
   q75, q25 = np.percentile(x,[75, 25])
   print("Mean                     : {}".format(np.mean(x)))
@@ -61,6 +69,6 @@ def measures(x):
 
 frequency_table(data, 20)
 histogram_plot(data, 20)
-bar_plot(data,20)
+bar_plot(data, 20)
 box_plot(data)
 measures(data)
