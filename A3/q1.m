fdbfb44e4150b165698eb17017c3311926e7fb56
rishitsanmukhani <rishitsanmukhani@gmail.com>
2017-04-27 23:46:22 +0530

    Complete

--- A3/q1.m
+++ A3/q1.m
@@ -5,6 +5,6 @@ fprintf('mean = %f\n', mean(data));
 fprintf('std  = %f\n', std(data));
 fprintf('var  = %f\n', var(data));
 
-H = histogram(data, 5:14)
-[D, PD] = allfitdist(data, 'PDF')
-[D, PD] = allfitdist(data, 'CDF')
+H = histogram(data, 5:14);
+[D, PD] = allfitdist(data, 'PDF');
+[D, PD] = allfitdist(data, 'CDF');
